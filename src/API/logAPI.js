e4572ed9e81c4aacfb6a38ab1f5e29b3e8ef7cab
marcuses101 <mnjconnolly@gmail.com>
2021-01-03 10:33:59 -0500

    add getLogs method

--- src/API/logAPI.js
+++ src/API/logAPI.js
@@ -2,6 +2,16 @@ import config from "../config";
 const url = `${config.SERVER}/log`;
 
 export const logAPI = {
+  async getLogs(){
+   try {
+     const response = await fetch(url);
+     if (!response.ok) throw new Error(response.statusText);
+     const logs = await response.json();
+     return logs;
+   } catch (error) {
+    console.error(error)
+   }
+  },
   async addElementLog(log){
     const response = await fetch(url, {
       method: 'POST',
