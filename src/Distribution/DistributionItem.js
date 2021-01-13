71a67755156cf03c553bfff0271266ff609f0185
marcuses101 <mnjconnolly@gmail.com>
2021-01-13 14:18:32 -0500

    adjust grid layout

--- src/Distribution/DistributionItem.js
+++ src/Distribution/DistributionItem.js
@@ -4,13 +4,13 @@ import './DistributionItem.css'
 
  export default function DistributionItem({name, badge, date, ribbon, distribution, dateDistributed}){
    return (
-     <li className="DistributionItem">
+     <>
         <span>{name} </span>
         {badge && <span className='badge'>{`Badge ${badge}`} </span>}
         {ribbon && <span className="ribbon">{ribbon} </span>}
         <span className='dateCompleted'> {dayjs(date).format('DD/MM/YYYY')} </span>
         {dateDistributed?<span>{dayjs(dateDistributed).format('DD/MM/YYYY')}</span>:<button onClick={distribution}>Distribute</button>}
-     </li>
+     </>
    )
  }
 
